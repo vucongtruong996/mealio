3f597cca3ecacf13365cefa12c7c1061f4dc940e
vucongtruong996 <vucongtruong996@gmail.com>
2026-06-21 23:06:54 +0700

    feat(menu): implement create category use case

--- mealio-menu-service/src/test/java/com/mealio/mealio_menu_service/domain/model/CategoryTest.java
+++ mealio-menu-service/src/test/java/com/mealio/mealio_menu_service/domain/model/CategoryTest.java
@@ -1,7 +1,6 @@
 package com.mealio.mealio_menu_service.domain.model;
 
 import org.junit.jupiter.api.Test;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -11,11 +10,7 @@ class CategoryTest {
     @Test
     void shouldCreateCategory() {
 
-        Category category = new Category(
-                UUID.randomUUID(),
-                "Breakfast",
-                "Morning meals"
-        );
+        Category category = Category.create("Breakfast", "Morning meals");
 
         assertThat(category.getName())
                 .isEqualTo("Breakfast");
@@ -25,11 +20,7 @@ class CategoryTest {
     void shouldThrowExceptionWhenNameIsBlank() {
 
         assertThatThrownBy(() ->
-                new Category(
-                        UUID.randomUUID(),
-                        "",
-                        "Description"
-                ))
+                Category.create("", "Description"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
